50975b80f9224506f67ad041bf82812a8f0dbbcf
Leo <stricak.leo@gmail.com>
2024-04-12 15:33:08 +0200

    Change application name to Imbus

--- backend/src/main/java/com/blitz/imbus/ImbusApplication.java
+++ backend/src/main/java/com/blitz/imbus/ImbusApplication.java
@@ -2,13 +2,12 @@ package com.blitz.whatsdown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class WhatsdownApplication {
+public class ImbusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WhatsdownApplication.class, args);
+		SpringApplication.run(ImbusApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/blitz/whatsdown/ImbusApplicationTests.java
+++ backend/src/test/java/com/blitz/whatsdown/ImbusApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WhatsdownApplicationTests {
+class ImbusApplicationTests {
 
 	@Test
 	void contextLoads() {
