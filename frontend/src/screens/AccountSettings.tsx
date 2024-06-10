369aa440c8b75e2d54b67b07d3fac5f4bdbfe51a
Leo <stricak.leo@gmail.com>
2024-06-10 09:00:01 +0200

    User edit

--- frontend/src/screens/AccountSettings.tsx
+++ frontend/src/screens/AccountSettings.tsx
@@ -10,156 +10,204 @@ import {UserData} from "@expo/config/build/getUserState";
 import {NavigationParameter} from "@/src/types/navigation/NavigationParameter";
 
 const defaultUserProfileImage = require("../../assets/icons/defaultUserProfile.png");
+import {colors} from "@/src/styles/colors";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import envVars from "@/src/utils/envVars";
 
 export const AccountSettings: React.FC<NavigationParameter> = ({ navigation }) => {
-    const [image, setImage] = useState("");
     const {userData, setUserData, dataLoading, refetchUserData } = userSessionData();
     const { updateUser:any, uploading, error } = updateUser({});
 
-    const [name, setName] = useState("");
-    const [surname, setSurname] = useState("");
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [role, setRole] = useState("");
-    const [location, setLocation] = useState("");
-    const [categories, setCategories] = useState([]);
+    const [hoverStates, setHoverStates] = useState({
+        premium: false,
+        referal: false,
+        profile: false,
+        settings: false,
+        logout: false,
+    });
+
+    const setHoverState = (key: any, value: any) => {
+        setHoverStates(prevState => ({ ...prevState, [key]: value }));
+    }
+
+    const logout= async () => {
+        await AsyncStorage.removeItem('token');
+        navigation.navigate('login');
+    }
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
-            aspect: [1, 1],
-            quality: 0.2,
+            aspect: [4, 3],
+            quality: 1,
+            base64: true,
         });
 
-        if (!result.canceled)
-            setImage(result.assets[0].uri);
-    };
+        if (!result.canceled) {
+            const base64Image = result.assets[0].base64;
+            // @ts-ignore
+            setUserData({ ...userData, profileImage: base64Image });
 
-    const handleSubmit = async () => {
-        if (!image) {
-            console.log("No image selected");
-            return;
-        }
+            if (base64Image != null) {
+                userData.attachment = base64Image;
 
-        try {
-            const formData:any = new FormData();
+                console.log(userData)
 
-            formData.append('image', {
-                uri: image,
-                type: 'image/jpeg',
-                name: 'profileImage.jpg'
-            });
+                const token = await AsyncStorage.getItem('token');
+                try {
+                    const response = await axios.put(`${envVars.API_ENDPOINT}/user/`, userData, {
+                        headers: { Authorization: `Bearer ${token}` }
+                    });
 
-            const user = {
-                name: name,
-                surname: surname,
-                username: username,
-                email: email,
-                password: password,
-                role: role,
-                location: location,
-                categories: categories
+                    return response.data;
+                } catch (error) {
+                    console.error(error);
+                }
+
             }
-
-            formData.append("user", JSON.stringify(user));
-
-            console.log('Uploading image:', formData)
-
-            updateUser(formData);
-        } catch (error) {
-            console.error('Info uploading image:', error);
         }
     };
-
-    useEffect(() => {
-        if (!userData) return;
-
-        /*
-        setName(userData.name);
-        setSurname(userData.surname);
-        setUsername(userData.username);
-        setEmail(userData.email);
-        setPassword(userData.password);
-        setRole(userData.role);
-        setLocation(userData.location);
-        setCategories(userData.categories);
-
-        setImage(`data:image/jpeg;base64,${userData.profileImage}`);
-*/    }, [dataLoading, userData]);
 
     return (
-        <View style={styles.container}>
-            <Header navigation={navigation} userData={userData}></Header>
-            {(!userData) && <ActivityIndicator style={styles.loaderFull} size="large" color="#0478ca" />}
-            <ScrollView style={styles.scrollContainer}>
-                {uploading && <ActivityIndicator style={styles.loader} size="large" color="#0478ca" />}
-                <View style={styles.profilePicture}>
-                    <Pressable style={styles.profilePictureImage} onPress={pickImage}>
-                        {image ? <Image source={{ uri: image }} style={styles.image} />
-                            : <Image source={defaultUserProfileImage} style={styles.image} />}
+        <View style={styles.containerBox}>
+            <Header navigation={navigation} userData={userData} />
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Pressable onPress={pickImage}>
+                        <Image style={styles.profileImage} source={{ uri: `data:image/jpeg;base64,${userData.profileImage}` }}/>
+                    </Pressable>
+                    <Text style={styles.fullName}>{userData.name} {userData.surname}</Text>
+                </View>
+                <View style={styles.boxContainer}>
+                    <Pressable style={[styles.box, styles.premiumBox, userData.role == 'CLIENT' ? {borderColor: "#0478ca"} : {borderColor: "#ffbf49"}, hoverStates.premium && colors.backgroundGray]}
+                               onPressIn={() => setHoverState("premium", true)}
+                               onPressOut={() => setHoverState("premium", false)}
+                    >
+                        <Text style={[styles.boxText, styles.premiumText, userData.role == 'CLIENT' ? colors.blue : colors.orange]}>Premium</Text>
+                        <Text style={[styles.boxText, styles.premiumText, userData.role == 'CLIENT' ? colors.blue : colors.orange]}>plan</Text>
+                    </Pressable>
+                    <Pressable style={[styles.box, hoverStates.referal && colors.backgroundGray]}
+                               onPressIn={() => setHoverState("referal", true)}
+                               onPressOut={() => setHoverState("referal", false)}
+                    >
+                        <Text style={styles.boxText}>Pozovi</Text>
+                        <Text style={styles.boxText}>prijatelja</Text>
                     </Pressable>
                 </View>
-                <UserForm
-                    name={name} setName={setName}
-                    surname={surname} setSurname={setSurname}
-                    username={username} setUsername={setUsername}
-                    email={email} setEmail={setEmail}
-                    password={password} setPassword={setPassword}
-                    role={role} setRole={setRole}
-                    categories={categories} setCategories={setCategories}
-                    location={location} setLocation={setLocation}
-                />
-                {error && <Text>Error: {error}</Text>}
-                <Button title="Submit" onPress={handleSubmit} />
-            </ScrollView>
+                <View style={styles.rectangleContainer}>
+                    <Pressable style={[styles.rectangle, hoverStates.profile && colors.backgroundGray]}
+                               onPressIn={() => setHoverState("profile", true)}
+                               onPressOut={() => setHoverState("profile", false)}
+                    >
+                        <Text style={styles.rectangleText}>Moj profil</Text>
+                    </Pressable>
+                    <Pressable style={[styles.rectangle, hoverStates.settings && colors.backgroundGray]}
+                               onPressIn={() => setHoverState("settings", true)}
+                               onPressOut={() => setHoverState("settings", false)}
+                    >
+                        <Text style={styles.rectangleText}>Postavke</Text>
+                    </Pressable>
+                    <Pressable style={[styles.rectangle, hoverStates.logout && colors.backgroundGray]}
+                               onPress={logout}
+                               onPressIn={() => setHoverState("logout", true)}
+                               onPressOut={() => setHoverState("logout", false)}
+                    >
+                        <Text style={styles.rectangleText}>Odjava</Text>
+                    </Pressable>
+                </View>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    containerBox: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+    },
     container: {
-        width: '100%',
-        height: '100%',
         flex: 1,
-    },
-    scrollContainer: {
-        flex: 1,
-    },
-    image: {
-        width: 200,
-        height: 200,
-    },
-    profilePicture: {
+        paddingTop: 50,
         alignItems: 'center',
-        marginTop: 30,
+        backgroundColor: '#ffffff',
     },
-    profilePictureImage: {
-        width: 200,
-        height: 200,
+    header: {
+        alignItems: 'center',
+    },
+    profileImage: {
+        width: 141,
+        height: 141,
         borderRadius: 100,
-        overflow: 'hidden',
+        backgroundColor: '#cccccc',
+        marginBottom: 10,
+    },
+    fullName: {
+        fontSize: 24,
+    },
+    boxContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '90%',
+        marginVertical: 20,
+    },
+    box: {
+        width: '48%',
+        height: 88,
+        backgroundColor: '#ffffff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+        borderStyle: 'solid',
         borderWidth: 1,
-        borderColor: '#000',
+        fontWeight: 'bold',
+        shadowColor: "#000000",
+        shadowOffset: {
+            width: 0,
+            height: 11,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 11.78,
+        elevation: 15
     },
-    loader: {
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        zIndex: 999,
-        backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    premiumText: {
+
+        color: '#FFBF49',
+    },
+    premiumBox: {
+        borderColor: '#FFBF49',
+    },
+
+    boxText: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    rectangleContainer: {
+        alignItems: 'center',
+        width: '100%'
+    },
+    rectangle: {
+        width: '90%',
+        height: 44,
+        backgroundColor: '#ffffff',
         justifyContent: 'center',
         alignItems: 'center',
+        borderRadius: 50,
+        marginVertical: 5,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        shadowColor: "#000000",
+        shadowOffset: {
+            width: 0,
+            height: 11,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 11.78,
+        elevation: 15
     },
-    loaderFull: {
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        zIndex: 999,
-        backgroundColor: "#f5f5f5",
-        paddingBottom: 50
-    }
+    rectangleText: {
+        fontSize: 20,
+    },
 });
